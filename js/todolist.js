9920357788c7c9c240de9745d5a4024f869e12ba
enrico <rombaldonienrico@gmail.com>
2020-12-10 18:24:58 +0100

    ishiden

--- js/todolist.js
+++ js/todolist.js
@@ -12,20 +12,20 @@ var app =
     data: {
       todolist: ["regalo anniversario","allenare Aron","finire grafica regalo", "scoprire problemi aggiornamento windows","preparare cose per pesca domenica","studiare inglese"],
       addItem:"",
+      isHidden: true,
     },
     methods: {
-      submit:function(){
-        this.addElement();
 
-
-      },
       // - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
       addElement:function(){
         this.todolist.push(this.addItem);
-          this.addItem="";
+        this.addItem="";
       },
       // - cancellare un item dalla lista, al click sulla relativa icona 'X'.
+      removeElement: function(index){
+        this.todolist.splice(index,1);
 
+      }
   }
 
 })
