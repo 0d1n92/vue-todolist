bb580e6aba86becad6c6b90a67a6fdfc462fbe6b
enrico <rombaldonienrico@gmail.com>
2020-12-10 14:18:07 +0100

    bonus

--- js/todolist.js
+++ js/todolist.js
@@ -1,7 +1,7 @@
 // cartella/repo di oggi: vue-todolist
 // Esercizio:
 
-// - cancellare un item dalla lista, al click sulla relativa icona 'X'.
+
 // Bonus:
 // - Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
 // Consiglio del giorno: per alcuni task, potrebbe essere necessario fare delle ricerche e capire come lavorano alcune funzioni o direttive leggendo le documentazioni ufficiali ed analizzando i relativi esempi.
@@ -14,10 +14,15 @@ var app =
       addItem:"",
     },
     methods: {
+      submit:function(){
+        console.log("test");
+        this.addElement();
+      },
       // - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
       addElement:function(){
         this.todolist.push(this.addItem);
       },
+      // - cancellare un item dalla lista, al click sulla relativa icona 'X'.
       removeElement: function(index){
         console.log(index);
         this.todolist.splice(index,1);
