d2d542d9d0baf63537c7e895118201417a6831bc
enrico <rombaldonienrico@gmail.com>
2020-12-10 21:43:07 +0100

    un lot of style

--- js/todolist.js
+++ js/todolist.js
@@ -10,12 +10,14 @@ var app =
   new Vue({
     el: '#app',
     data: {
-      todolist: ["regalo anniversario","allenare Aron","finire grafica regalo", "scoprire problemi aggiornamento windows","preparare cose per pesca domenica","studiare inglese"],
+      todolist: ["allenare Aron", "scoprire problemi aggiornamento windows","preparare cose per pesca domenica","studiare inglese"],
       addItem:"",
       isHidden: true,
     },
     methods: {
-
+      submit:function () {
+          this.addElement()
+      },
       // - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
       addElement:function(){
         this.todolist.push(this.addItem);
